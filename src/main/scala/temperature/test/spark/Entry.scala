4dd7a5b24b5bd1f14fcd316128cd37a24c6f1350
Eugene <eugene@Eugenes-MacBook-Pro.local>
2019-09-30 21:07:28 +0300

    implement records field for the Spark

--- src/main/scala/temperature/test/spark/Entry.scala
+++ src/main/scala/temperature/test/spark/Entry.scala
@@ -1,15 +1,57 @@
 package temperature.test.spark
 
-import temperature.test.model.{MeteoRecord, MonthlyAverage}
+import java.sql.Date
+import org.apache.spark.sql.Dataset
+import temperature.test.spark.model.{MeteoRecord, MonthlyAverage}
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 
 object Entry {
 
   def main(args: Array[String]) {
+    val spark = SparkSession.builder.appName("Temperature Test")
+      .config("spark.master", "local")
+      .getOrCreate()
 
     /**
      * Dataset of temperature.csv
      */
-    val records: AnyRef = ??? // Dataset[MeteoRecord]
+    val records: Dataset[MeteoRecord] = {
+      import spark.implicits._
+
+      val df = spark
+        .read
+        .options(Map(
+          "header" -> "true",
+          "timestampFormat" -> "yyyy-MM-dd",
+          "inferSchema" -> "true"
+        ))
+        .csv("src/main/resources/temperature.csv")
+        .map(field => {
+          // todo should refactor this part of code, maybe possible to immediately map to the model
+          import java.text.SimpleDateFormat
+          val format = new SimpleDateFormat("yyyy-MM-dd")
+          val date = format.parse(field(11).toString)
+          val latitude = field(5).toString
+          val longitude = field(6).toString
+          val measurement = field(13).asInstanceOf[Double]
+          val stateName = field(21).toString
+          val countyName = field(22).toString
+
+            MeteoRecord(
+              new Date(date.getTime),
+              latitude.toDouble,
+              longitude.toDouble,
+              Some(measurement),
+              stateName,
+              countyName
+            )
+
+        })
+        .as[MeteoRecord]
+
+      df
+    }
 
     println(
       "Months from hottest to coldest:" +
